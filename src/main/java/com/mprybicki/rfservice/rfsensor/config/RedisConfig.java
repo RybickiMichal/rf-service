8bc36cd6a06da6c8b6c2a7622de6be8e3d821db6
RybickiMichal <rybickimichal2@gmail.com>
2021-01-17 14:54:33 +0100

    sensor-apps changes to work on docker compose

--- src/main/java/com/mprybicki/rfservice/rfsensor/config/RedisConfig.java
+++ src/main/java/com/mprybicki/rfservice/rfsensor/config/RedisConfig.java
@@ -1,22 +1,60 @@
 package com.mprybicki.rfservice.rfsensor.config;
 
-
+import io.lettuce.core.ClientOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
 public class RedisConfig {
-    @Bean
-    JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
+
+    private final String url;
+    private final int port;
+
+    public RedisConfig(@Value("${spring.redis.host}") String url,
+                       @Value("${spring.redis.port}") int port) {
+        this.url = url;
+        this.port = port;
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory());
+    public RedisStandaloneConfiguration redisStandaloneConfiguration() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(url, port);
+        return redisStandaloneConfiguration;
+    }
+
+    @Bean
+    public ClientOptions clientOptions() {
+        return ClientOptions.builder()
+                .disconnectedBehavior(ClientOptions.DisconnectedBehavior.REJECT_COMMANDS)
+                .autoReconnect(true)
+                .build();
+    }
+
+    @Bean
+    public RedisConnectionFactory connectionFactory(RedisStandaloneConfiguration redisStandaloneConfiguration) {
+
+        LettuceClientConfiguration configuration = LettuceClientConfiguration.builder()
+                .clientOptions(clientOptions()).build();
+
+        return new LettuceConnectionFactory(redisStandaloneConfiguration, configuration);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(name = "redisTemplate")
+    @Primary
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        StringRedisTemplate template = new StringRedisTemplate();
+        template.setConnectionFactory(redisConnectionFactory);
         return template;
     }
+
 }
